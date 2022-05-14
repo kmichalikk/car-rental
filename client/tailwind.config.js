e378b3373b4895a1deb9878f8a7acc6b5a8718da
Konrad Michalik <kmichalikk19@gmail.com>
2022-05-14 20:21:19 +0200

    Disable tailwind's purge option

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,41 +1,43 @@
 module.exports = {
   purge: {
-    enabled: !process.env.ROLLUP_WATCH,
-    mode: 'all',
-    content: ['./**/**/*.html', './**/**/*.svelte'],
+    enabled: false,
+    mode: "all",
+    content: ["./**/**/*.html", "./**/**/*.svelte"],
 
     options: {
       whitelistPatterns: [/svelte-/],
       defaultExtractor: (content) =>
-        [...content.matchAll(/(?:class:)*([\w\d-/:%.]+)/gm)].map(([_match, group, ..._rest]) => group),
+        [...content.matchAll(/(?:class:)*([\w\d-/:%.]+)/gm)].map(
+          ([_match, group, ..._rest]) => group
+        ),
     },
   },
   darkMode: false, // or 'media' or 'class'
-    theme: {
+  theme: {
     screens: {
-      'xs': '380px',
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
+      xs: "380px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
     },
     extend: {
       spacing: {
-        '100': '25rem',
-        '104': '26rem',
-        '108': '27rem',
-        '112': '28rem',
-        '116': '29rem',
-        '120': '30rem',
-        '124': '31rem',
-        '128': '32rem',
-        '132': '33rem',
-      }
-    }
+        100: "25rem",
+        104: "26rem",
+        108: "27rem",
+        112: "28rem",
+        116: "29rem",
+        120: "30rem",
+        124: "31rem",
+        128: "32rem",
+        132: "33rem",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
